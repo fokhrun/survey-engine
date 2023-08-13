9b63bd9a9f6b7868e11d840beb1c83f71f211dab
Fakhrunnahar Rupa <rupa0609@gmail.com>
2023-08-13 20:55:08 +0200

    Make the app to take user input

--- run.py
+++ run.py
@@ -3,4 +3,10 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 if __name__ == "__main__":
-    print ("Hello World!")
+    option = int(input("Enter your choice: "))
+    range_min = 1
+    range_max = 3
+    if option not in (1, 3):
+        raise ValueError(f"the value must be between {range_min, range_max}")
+    else:
+        print (f"Input is {option}")
