c45fbd65e4dbb66084f729b6e29f9d5a7a73aa6d
Fakhrunnahar Rupa <rupa0609@gmail.com>
2023-08-13 21:44:13 +0200

    add print and multiple choice options

--- survey-engine/survey.py
+++ survey-engine/survey.py
@@ -15,7 +15,7 @@ class Question:
         self.question_text = question_text
         self.response_options = []
 
-    def add_response_option(self, option: list[any]):
+    def add_response_option(self, options: list[any]):
         """add response options to the question
 
         Parameters
@@ -23,4 +23,11 @@ class Question:
         options : list[any]
             response options
         """
-        self.response_options.append(option)
+        self.response_options = options
+
+    def print_question(self):
+        """print question in a terminal"""
+        print(self.question_text)
+        if not self.response_options:
+            for idx, choice in enumerate(self.response_options, start=1):
+                print(f"{idx}. {choice}")
